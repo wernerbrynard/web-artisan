a789070bd07aca288d30defa4463a82d05a35ae1
Werner Brynard <werner.brynard@gmail.com>
2025-01-08 12:12:25 +0200

    Updated seo by adding sitemap and robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: 'https://webartisan.co.za',
+    generateRobotsTxt: true,
+    changefreq: 'weekly',
+    priority: 0.7,
+    generateIndexSitemap: false, 
+    sitemapSize: 5000, 
+};
