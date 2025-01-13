b9efccb2d8abec7413fca87c5b43913c037cab4c
Werner Brynard <werner.brynard@gmail.com>
2025-01-13 15:50:44 +0200

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,17 @@
 // src/components/Footer.js
-import { Box, Text, Link } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 const Footer = () => {
-    return (
-        <Box as="footer" py={4} bg="var(--main-color)" color="white" textAlign="center">
-            <Text fontSize="sm">&copy; {new Date().getFullYear()} Web Artisan. All rights reserved.</Text>
-            {/* <Text fontSize="sm">
-                <Link href="/about-werner" color="white" _hover={{ color: "var(--accent-color)" }}>
-                    About
-                </Link>
-            </Text> */}
-        </Box>
-    );
+  return (
+    <Box as="footer" py={4} bg="var(--main-color)" color="white" textAlign="center">
+      <Text fontSize="sm">
+        &copy; {new Date().getFullYear()} Web Artisan Studios (PTY) LTD. All rights reserved.
+      </Text>
+      <Text fontSize="xs" mt={2}>
+        Registration No: 2025/012325/07
+      </Text>
+    </Box>
+  );
 };
 
 export default Footer;
